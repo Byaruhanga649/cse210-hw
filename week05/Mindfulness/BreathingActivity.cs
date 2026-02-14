abcb6934ab3f3bf19f0792104271b6fc77003dc2
yourusername <abyaruhanga@byupathway.edu>
2026-02-14 20:25:30 +0300

    Implement mindfulness activities: add Breathing, Reflection, and Listing activities with menu options

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+        : base("Breathing",
+        "This activity will help you relax by guiding you through slow breathing.")
+    { }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("\nBreathe in... ");
+            ShowCountdown(4);
+
+            Console.Write("\nBreathe out... ");
+            ShowCountdown(4);
+        }
+
+        DisplayEndingMessage();
+    }
+}
